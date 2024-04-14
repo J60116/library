8115346fe876ceb86fca83865010dd25d31a7e85
mow <j16.gleen8@gmail.com>
2024-04-14 21:08:04 +0900

    ルーティングの修正

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TopController;
 
 Route::get('/welcome', function () {
     return view('welcome');
@@ -9,3 +10,10 @@ Route::get('/welcome', function () {
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/login',function () {
+    return view('login');
+});
+
+Route::get('/login',[TopController::class,'loginCheck']);
+Route::post('/library/index',[TopController::class,'login']);
